8d16638957ddd9d0ecb2bbc070db893d03c3d915
SaMucsPoP <116589388+SaMucsPoP@users.noreply.github.com>
2023-04-03 13:19:02 -0300

    att

--- Programação/C/WIFI_STEP_MOTOR_ESP32/BACKCODE/EDITAR_LIVRE/EDITAR_LIVRE.ino
+++ Programação/C/WIFI_STEP_MOTOR_ESP32/BACKCODE/EDITAR_LIVRE/EDITAR_LIVRE.ino
@@ -97,7 +97,7 @@ void handlemenu()
   else if(BValor)
   {
     html += "<style>*{margin: 0px; padding: 0px; box-sizing: border-box;  font-family: Arial, Helvetica, sans-serif;}";
-    html += "body, htmlbackground: linear-gradient(45deg, rgb(4, 23, 39), rgb(37, 51, 112)); height: 100vh; width: 100vw;}";
+    html += "body, html {background: linear-gradient(45deg, rgb(4, 23, 39), rgb(37, 51, 112)); height: 100vh; width: 100vw;}";
     html += "h1{color: white; position: absolute; width: 270px; margin-top: 45%; margin-left: 22%;}";
     html += "form > button{font-size: 1.9em; background-color: rgb(154, 191, 212); position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); height: 300px; width: 300px; border-radius: 50%; border: none;}</style>";
     html += "<body><h1>Controle do Motor</h1>";
