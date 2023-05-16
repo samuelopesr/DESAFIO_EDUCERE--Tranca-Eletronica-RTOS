63d7f9fb09bcb8564758392f5358c0797387bcbb
ArtumosGOC <staus334@gmail.com>
2023-05-16 15:07:55 -0300

    deletando arquivos inuteis

--- Programação/C/MultTask-Prog(modified)/Wifi_com_multi_processamento/VoidWifi_HandleHome.ino
+++
@@ -1,7 +0,0 @@
-void voidwifihandlehome(void * pvParameters ) 
-{
-  String html = "<html><head><title>ESP32 Online!!</title></head><body><a href='./Chave'>MENU</a></body></html>";
-
-  //manda informação para o servidor web
-  Servidor.send(200, "text/html", html);
-}
