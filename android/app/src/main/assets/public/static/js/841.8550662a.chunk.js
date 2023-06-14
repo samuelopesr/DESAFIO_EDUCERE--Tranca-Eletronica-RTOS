37b8d708d4e557561d168f384526944f7a07a481
munir <116471912+Munircampagnoli@users.noreply.github.com>
2023-06-14 16:25:01 -0300

    att

---
+++ android/app/src/main/assets/public/static/js/841.8550662a.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 841.8550662a.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[841],{841:(e,t,n)=>{n.r(t),n.d(t,{startStatusTap:()=>a});var s=n(554),r=n(500),o=n(811);const a=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,s.wj)((()=>{const t=e.innerWidth,n=e.innerHeight,a=document.elementFromPoint(t/2,n/2);if(!a)return;const i=(0,r.a)(a);i&&new Promise((e=>(0,o.c)(i,e))).then((()=>{(0,s.Iu)((async()=>{i.style.setProperty("--overflow","hidden"),await(0,r.s)(i,300),i.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=841.8550662a.chunk.js.map
